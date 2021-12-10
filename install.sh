e58640c2d92f39fd47e5c5210be355a201dfbf8a
ohyama4z <45777685+ohyama4z@users.noreply.github.com>
2021-12-10 20:39:34 +0900

    zinitに乗り換え

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# https://qiita.com/ganariya/items/d9adffc6535dfca6784bより
+
+# 未定義な変数があったら途中で終了する
+set -u
+
+# 今のディレクトリ
+# dotfilesディレクトリに移動する
+BASEDIR=$(dirname $0)
+cd $BASEDIR
+
+# dotfilesディレクトリにある、ドットから始まり2文字以上の名前のファイルに対して
+for f in .??*; do
+    [ "$f" = ".git" ] && continue
+    [ "$f" = ".gitconfig.local.template" ] && continue
+    [ "$f" = ".gitmodules" ] && continue
+
+    # シンボリックリンクを貼る
+    ln -snfv ${PWD}/"$f" ~/
+done
