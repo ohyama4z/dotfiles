ec812a59bc8f978a91e240b6b33cf82de5e08936
ohyama4z <45777685+ohyama4z@users.noreply.github.com>
2022-09-19 16:26:13 +0900

    [update] windows側のクリップボードと共有

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -31,3 +31,12 @@
 
 ;; カラーテーマの設定
 (load-theme 'deeper-blue t)
+
+;; Windowsのクリップボードと共有(WSL向け)
+(defun wsl-copy (start end)
+  (interactive "r")
+  (shell-command-on-region start end "clip.exe"))
+
+(global-set-key
+ (kbd "C-c C-c")
+  'wsl-copy)
